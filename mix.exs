9c5e5f465c637f31c882bf23ba9cffd6342c2e94
Aleksei Magusev <lexmag@me.com>
2021-09-21 03:02:28 +0200

    Prepare the project file for releasing

--- mix.exs
+++ mix.exs
@@ -1,14 +1,24 @@
 defmodule Cashmere.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/lexmag/cashmere"
+  @version "0.1.0"
+
   def project() do
     [
       app: :cashmere,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: description(),
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      name: "Cashmere",
+      docs: [
+        main: "Cashmere",
+        source_ref: "v#{@version}",
+        source_url: @source_url
+      ]
     ]
   end
 
@@ -24,5 +34,13 @@ defmodule Cashmere.MixProject do
 
   defp deps() do
     [{:ex_doc, "~> 0.24", only: :dev, runtime: false}]
+  end
+
+  defp package() do
+    [
+      maintainers: ["Aleksei Magusev"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    ]
   end
 end
